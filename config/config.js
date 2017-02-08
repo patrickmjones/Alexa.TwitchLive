62585254e4e55b7f973f20ad7c911fb5824d60c7
Patrick Jones <patrick@patrickmjones.com>
2017-02-08 03:13:38 -0500

    Added configuration vars
    Configuration vars added to config objects

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
 /* Update configuration accordingly */
 var config = {
     // Change to Lambda application id
+    AppID:      "",
 
     // Change to Twitch client id
+    ClientID:   "",
 
     // Change to Twitch username
+    UserID:     ""   
 }
 
 module.exports = exports = config;
